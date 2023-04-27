25475b38c2d9e1826d11bbd991bb62d9aa9bac53
SergioAi <53959551+Spai26@users.noreply.github.com>
2023-04-27 00:20:17 -0500

    header navigation and action

--- components/navigation/header.jsx
+++ components/navigation/header.jsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 import styles from "./Header.module.css";
 import icon from "../assets/flat,128x128,075,t.jpg";
 import MenuItems from "./itemsNav";
-
+import { RxHamburgerMenu, RxCross1 } from "react-icons/rx";
 import { socials } from "../utils/data";
 
 export default function Header({ blog }) {
@@ -23,65 +23,70 @@ export default function Header({ blog }) {
     return () => {
       document.removeEventListener("mousedown", handleClickOutside);
     };
-  }, [setSideBarToogle]);
+  }, [sideBarToogle]);
 
   return (
-    <Fragment>
-      {/* nav movile */}
-      <div className={styles.mov_header}>
-        <div className={styles.c_flex}>
-          <div className={styles.navbar}>
-            <Link href="/">SergioAi</Link>
-          </div>
-          {/* toogle button */}
-          <button
-            className={`${styles.toogle_menu} ${sideBarToogle ? "open" : ""}`}
-            onClick={() => setSideBarToogle(!sideBarToogle)}
-          >
-            <span />
-            <span />
-            <span />
-          </button>
-        </div>
-      </div>
-      {/* end nav movile */}
-
-      {/* navbar */}
-      <header
-        className={`${styles.header_left} ${
-          sideBarToogle ? `${styles.menu_open}` : ""
-        }`}
-      >
-        <div className={styles.scroll_bar}>
-          <div className={styles.hl_top}>
-            <div className={styles.hl_logo}>
-              <div className={styles.img}>
-                <Image
-                  src={icon}
-                  alt="me icon"
-                  unoptimized={true}
-                  priority={true}
-                />
-              </div>
-              <h5>SergioAi</h5>
+    console.log(sideBarToogle),
+    (
+      <Fragment>
+        {/* nav movile */}
+        <div className={styles.mov_header}>
+          <div className={styles.c_flex}>
+            <div className={styles.navbar}>
+              <Link href="/">SergioAi</Link>
             </div>
-          </div>
-          {blog ? "" : <MenuItems />}
-        </div>
-
-        <div className={styles.socials_icon}>
-          {socials.map((social, index) => (
-            <a
-              key={index}
-              target="_blank"
-              rel="noopener noreferrer"
-              href={social.url}
+            {/* toogle button */}
+            <button
+              className={`${styles.toogle_menu} `}
+              onClick={() => setSideBarToogle(!sideBarToogle)}
             >
-              {social.svg}
-            </a>
-          ))}
+              {sideBarToogle ? (
+                <RxCross1 className={styles.base} />
+              ) : (
+                <RxHamburgerMenu className={styles.base} />
+              )}
+            </button>
+          </div>
         </div>
-      </header>
-    </Fragment>
+        {/* end nav movile */}
+
+        {/* navbar */}
+        <header
+          className={`${styles.header_left} ${
+            sideBarToogle ? `${styles.menu_open}` : ""
+          }`}
+        >
+          <div className={styles.scroll_bar}>
+            <div className={styles.hl_top}>
+              <div className={styles.hl_logo}>
+                <div className={styles.img}>
+                  <Image
+                    src={icon}
+                    alt="me icon"
+                    unoptimized={true}
+                    priority={true}
+                  />
+                </div>
+                <h5>SergioAi</h5>
+              </div>
+            </div>
+            {blog ? "" : <MenuItems />}
+          </div>
+
+          <div className={styles.socials_icon}>
+            {socials.map((social, index) => (
+              <a
+                key={index}
+                target="_blank"
+                rel="noopener noreferrer"
+                href={social.url}
+              >
+                {social.svg}
+              </a>
+            ))}
+          </div>
+        </header>
+      </Fragment>
+    )
   );
 }
