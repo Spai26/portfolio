b5fff29a28550f9bba9208e49b423a4e0c8c2849
SergioAi <53959551+Spai26@users.noreply.github.com>
2023-05-02 18:54:16 -0500

    main loader

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,5 +1,18 @@
+import { useEffect, useState } from "react";
 import "../src/styles/app.css";
+import PreLoader from "@/src/components/layout/utils/preloader";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  const [load, setLoad] = useState(true);
+  useEffect(() => {
+    setTimeout(() => {
+      setLoad(false);
+    }, 1000);
+  }, []);
+  return (
+    <>
+      {load && <PreLoader />}
+      <Component {...pageProps} />
+    </>
+  );
 }
