fab5493431731e14adc512b072176d7b56e17d31
SergioAi <53959551+Spai26@users.noreply.github.com>
2023-05-02 21:35:00 -0500

    scrolling section

--- src/components/navigation/itemsNav.jsx
+++ src/components/navigation/itemsNav.jsx
@@ -7,7 +7,20 @@ const MenuItems = () => {
   const [scrolled, setScrolled] = useState(false);
   useEffect(() => {
     const onScroll = () => {
-      if (window.scrollY > 50) {
+      const scrollPosition = window.scrollY;
+      const sections = document.querySelectorAll("section");
+
+      sections.forEach((section) => {
+        const sectionTop = section.offsetTop - 50;
+        const sectionBottom = sectionTop + section.offsetHeight / 3;
+
+        if (scrollPosition >= sectionTop && scrollPosition < sectionBottom) {
+          const id = section.getAttribute("id");
+          setActiveLink(id);
+        }
+      });
+
+      if (scrollPosition > 50) {
         setScrolled(true);
       } else {
         setScrolled(false);
@@ -23,12 +36,12 @@ const MenuItems = () => {
     setActiveLink(value);
   };
   return (
-    <ul className={`${styles.nav_menu} ${scrolled ? "scrolled" : ""}`}>
+    <ul className={`${styles.nav_menu} ${scrolled ? "scrolled" : ""} `}>
       {navigations.map((items) => (
         <li key={items.id}>
           <a
             className={`${styles.nav_link} ${
-              activeLink === items.href ? styles.active : ""
+              activeLink === items.href.substring(1) ? styles.active : ""
             }`}
             onClick={() => updateActive(items.href)}
             href={items.href}
